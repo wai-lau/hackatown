ce92a230703d56f6cc6dfb29a7f092282c4d3c74
Wai <12164553+Wai-Lau@users.noreply.github.com>
2018-01-20 16:38:13 -0500

    added map code

--- request.py
+++ request.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     message = "WHY WHY MANTI"
-    return render_template("main.html", message=message)
+    api_key = 'AIzaSyAl-P2j3M-a-IjP7Vfkp_ChinCQMTsb__0'
+    return render_template("map.html", message=message, api_key=api_key)
