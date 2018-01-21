795f34962d6d3a3a72a4a6c2c99395c47ac21e25
fredriclam <fredriclam@live.com>
2018-01-21 04:54:22 -0500

    Added some front-end stuff for polygons.

--- static/map.js
+++ static/map.js
@@ -1,6 +1,7 @@
 let KEY = ''
 let MAP = null
 let MARKERS = {}
+let STATE = 'polygon'
 
 initMap = (markers, key) => {
   KEY = key;
@@ -11,15 +12,20 @@ initMap = (markers, key) => {
   MARKERS = markers;
   addClickListener();
   loadAllMarkers();
+
+  // Initialize polygon list and pushes the initial polygon
+  polygonList = [];
+  polygonList.push(new PolygonWrapper(MAP));
    //setInterval(() => { alert("Hello"); }, 3000);
 }
 
 addClickListener = () => {
   MAP.addListener('click', (e) => {
-    placeMarkerAndPanTo(e.latLng, MAP);
     let name = Date.now() + Math.random();
     console.log(name)
-    $.ajax({
+    if(STATE == 'marker'){
+      placeMarkerAndPanTo(e.latLng, MAP);
+      $.ajax({
         url: '/add_marker/' + KEY,
         type: 'post',
         dataType: 'json',
@@ -33,7 +39,54 @@ addClickListener = () => {
           console.log(xhr);
         }
       });
-   });
+    }
+    else if(STATE == 'polygon'){
+      // Polygon logic for first polygon; draws until state is off
+      if (polygonList[0].state == 'draw'){
+        polygonList[0].addNode(e.latLng);
+        polygonList[0].updatePolygon();
+
+        $.ajax({
+          url: '/add_polygon/' + KEY,
+          type: 'post',
+          dataType: 'json',
+          contentType: 'application/json',
+          data: JSON.stringify({'latLng': JSON.stringify(polygonList[0].pointList), 'name':name ,'color':polygonList[0].color}),
+          success: function (xhr) {
+            console.log(xhr)
+          },
+          error: function(xhr) {
+            console.error(xhr);
+          }
+        });
+      }
+    }
+  });
+
+  MAP.addListener('rightclick', (e) => {
+    let name = Date.now() + Math.random();
+    console.log(name);
+    if(STATE == 'polygon'){
+      // Make new polygon wrapper
+      polygonList.push(new PolygonWrapper(MAP));
+      // Locks previous polygon from left click draw
+      polygonList[polygonList.length-2].state = 'locked';
+
+      $.ajax({
+        url: '/add_polygon/' + KEY,
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify({'latLng': JSON.stringify(polygonList[polygonList.length-1].pointList), 'name':name, 'color':polygonList[0].color}),
+        success: function (xhr) {
+          console.log(xhr)
+        },
+        error: function(xhr) {
+          console.error(xhr);
+        }
+      });
+    }
+  });
 }
 
 loadAllMarkers = () => {
