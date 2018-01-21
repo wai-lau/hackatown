e8e74e1983d08257b1e0e76e023ff81caf0fb2c2
Kevin Tran <kevin.tran@yuvo.io>
2018-01-20 20:41:43 -0500

    request.py changed

--- request.py
+++ request.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    message = "guugufffg"
+    message = "guuguffg"
     api_key = 'AIzaSyAl-P2j3M-a-IjP7Vfkp_ChinCQMTsb__0'
     return render_template("map.html", message=message, api_key=api_key)
 
