1a02f4b4b3e7ef76fc31e86cc1fd530631d1fced
fredriclam <fredriclam@live.com>
2018-01-21 06:51:00 -0500

    Polygon rendering and communication to backend

--- request.py
+++ request.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 hash_data = {}
+polygon_hash_data = {}
 
 @app.route('/group/<key>')
 def group(key):
@@ -41,3 +42,17 @@ def create_group():
     key = m.hexdigest().encode('utf-8').strip()
     hash_data[key] = {}
     return json.dumps({'success':True, 'key': key}), 200, {'ContentType':'application/json'}
+
+@app.route('/add_polygon/<key>', methods=['POST'])
+def add_polygon(key):
+    # Get name (hash ID) of polygon
+    name = request.get_json()['name']
+    print(name)
+    data = request.get_json()['data']
+    # ptList_json = request.get_json()['pointList']
+    # color = request.get_json()['color']
+    if key not in polygon_hash_data:
+        polygon_hash_data[key] = {}
+    polygon_hash_data[key][name] = data
+    print(data)
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
--- static/PolygonWrapper.js
+++ static/PolygonWrapper.js
@@ -5,6 +5,7 @@ class PolygonWrapper {
     this.polygon = null;
     this.map = map;
     this.color = '#FF00FF';
+    this.name = Date.now() + Math.random();
   }
 
   kill() {
@@ -26,23 +27,23 @@ class PolygonWrapper {
     this.kill();
 
     // Draw polygon
-    // Do not draw front-end
-    // this.polygon = new google.maps.Polygon({
-    //   paths: this.pointList,
-    //   strokeColor: this.color,
-    //   strokeOpacity: 0.8,
-    //   strokeWeight: 2,
-    //   fillColor: this.color,
-    //   fillOpacity: 0.25,
-    //   draggable: true
-    // });
-    //
-    // this.polygon.addListener('rightclick', () => {if (this.polygon)
-    //   this.kill();
-    //   this.resetPointList();
-    //   });
-    //
-    // // Place on map
-    // this.polygon.setMap(this.map);
+    this.polygon = new google.maps.Polygon({
+      paths: this.pointList,
+      strokeColor: this.color,
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: this.color,
+      fillOpacity: 0.25,
+      draggable: true
+    });
+
+    this.polygon.addListener('rightclick', () => {if (this.polygon)
+      this.kill();
+      this.resetPointList();
+      this.state = 'deleted';
+      });
+
+    // Place on map
+    this.polygon.setMap(this.map);
   }
 }
